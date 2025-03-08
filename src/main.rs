ed798c67f325077eb5f1039e9511f4fa1cfe8ba2
beklopft <ghub@giggo.de>
2025-03-08 15:57:47 +0100

    Set target path

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,7 @@ fn main() {
 
     let mut hash_gen = HashGen::new(hash);
     let fields = Fields::from_hash(&mut hash_gen);
+
+    let mut target_file = std::path::PathBuf::from("./icons");
+    target_file.push(hash.to_string());
 }
