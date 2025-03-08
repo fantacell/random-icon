a6316bda183060043d986b29f7283464febc2a35
beklopft <ghub@giggo.de>
2025-03-08 15:39:44 +0100

    Add Fields generation, compiling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use random_icon::hash::hash;
+use random_icon::{
+    fields::Fields,
+    hash::hash,
+    random::{FromHash, HashGen}
+};
 
 fn main() {
     let string = std::env::args().nth(1).unwrap_or_default();
     let hash = hash(string);
+
+    let mut hash_gen = HashGen::new(hash);
+    let fields = Fields::from_hash(&mut hash_gen);
 }
