be493d5bd3bd164c7fef191238fc3dda5622c3da
beklopft <ghub@giggo.de>
2025-03-09 00:33:11 +0100

    Add svg data for a few fields

--- src/svg/sector_field.rs
+++ src/svg/sector_field.rs
@@ -11,10 +11,24 @@ pub enum SectorField {
 
 impl FieldShape for SectorField {
     fn svg_path_data(self) -> path::Data {
-        Data::new()
-            .move_by((20.652, 11.923))
-            .vertical_line_by(11.932)
-            .elliptical_arc_by((33.324, 33.324, 0, 0, 0, -20.651, -7.17))
-            .close()
+        match self {
+            SectorField::Outer => {
+                Data::new()
+                .move_to((0, -50))
+                .elliptical_arc_to((50, 50, 0, 0, 1, 42.267, -26.712))
+                .line_to((27.808, -18.364))
+                .elliptical_arc_to((33.324, 33.324, 0, 0, 0, 27.808, -18.364))
+                .vertical_line_to(-38.077)
+                .close()
+            },
+            SectorField::OuterMid => {
+                Data::new()
+                .move_to((0, -50))
+                .line_to((20.651, -38.077))
+                .vertical_line_to(-26.155)
+                .elliptical_arc_to((33.324, 33.324, 0, 0, 0, 0, 33.324))
+                .close()
+            }
+        }
     }
 }
