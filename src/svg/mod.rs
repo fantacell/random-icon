0573f500db46a728d1931e60b0dd66950ac6407a
beklopft <ghub@giggo.de>
2025-03-10 12:04:47 +0100

    Write wanted functionality

--- src/svg/mod.rs
+++ src/svg/mod.rs
@@ -7,12 +7,24 @@ pub mod path;
 pub mod sector_field;
 
 impl Fields {
-    pub fn save_as_svg_file<L: AsRef<Path>>(self, location: L) {
-        let document = svg::Document::new()
+    pub fn save_as_svg_file(self, location: std::path::PathBuf) {
+        let mut document = svg::Document::new()
             .set("viewBox", (-50, -50, 50, 50))
             .set("width", "100")
             .set("height", "100")
         ;
+        
+        for (field_shape, degrees) in self.active_field_shapes_with_rotation() {
+            let path_element = svg::node::element::Path::new()
+                .set("fill", "black")
+                .set("d", field_shape)
+                .set("transform", format!("rotate({}, 0, 0)", degrees))
+            ;
+
+            document = document.add(path_element)
+        }
+
+        svg::save(location, &document).unwrap();
     }
 }
 
