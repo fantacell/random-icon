8a79836307e2dc1708af50381e30e30954149f71
beklopft <ghub@giggo.de>
2025-03-18 21:31:51 +0100

    Move binary logic to library

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,21 +1,14 @@
-use random_icon_lib::{
-    fields::Fields,
-    hash::hash,
-    random::{FromHash, HashGen}
-};
+use random_icon_lib::save_random_icon_from_hashed_string;
 
 fn main() {
-    let string = std::env::args().nth(1).unwrap_or_default();
-    let hash = hash(string);
+    let string = std::env::args()
+        .nth(1)
+        .unwrap_or("default".to_string())
+    ;
 
-    let mut hash_gen = HashGen::new(hash);
-    let fields = Fields::from_hash(&mut hash_gen);
-
-    let target_directory = std::path::PathBuf::from("./icons");
-    let target_file_name = hash.to_string();
-
-    let mut target_path = target_directory;
-    target_path.push(target_file_name);
-
-    fields.save_as_svg_file(target_path);
+    save_random_icon_from_hashed_string(
+        string,
+        "./icons".into(),
+        None
+    );
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,27 @@
+use std::path::PathBuf;
+
+use fields::Fields;
+use hash::hash;
+use random::{FromHash, HashGen};
+
 pub mod fields;
 pub mod hash;
 pub mod random;
 pub mod svg;
 pub mod tests;
+
+pub fn save_random_icon_from_hashed_string(
+    string: String,
+    target_dir: PathBuf,
+    filename: Option<String>
+) {
+    let hash = hash(string);
+
+    let mut hash_gen = HashGen::new(hash);
+    let fields = Fields::from_hash(&mut hash_gen);
+
+    let filename = filename.unwrap_or(hash.to_string());
+    let target_path = target_dir.join(filename);
+
+    fields.save_as_svg_file(target_path);
+}
