de44a2001bc21f9a8133fe69ca883f51ec8234c3
beklopft <ghub@giggo.de>
2025-03-20 15:52:25 +0100

    Restore to original shape

--- lib/src/generate/from_hash.rs
+++ lib/src/generate/from_hash.rs
@@ -8,6 +8,10 @@ pub struct HashGen {
     hash: u32
 }
 
+pub trait FromHash {
+    fn from_hash(hashgen: &mut HashGen) -> Self;
+}
+
 impl HashGen {
     pub fn new(hash: u32) -> Self {
         Self { hash }
@@ -35,99 +39,99 @@ impl HashGen {
     }
 }
 
-//impl FromHash for Fields {
-//    fn from_hash(hash_gen: &mut HashGen) -> Self {
-//        let symmetry = Symmetry::from_hash(hash_gen);
-//        let center_field: Field = Field::from_hash(hash_gen);
-//
-//        match symmetry {
-//            Symmetry::OneAxis => {
-//                let sectors: [_; 3] = array::from_fn(|_| Sector::from_hash(hash_gen));
-//                let sector_dividers: [_; 3] = array::from_fn(|_| SectorDivider::from_hash(hash_gen));
-//
-//                let all_sectors = [sectors[0], sectors[1], sectors[2], sectors[2], sectors[1], sectors[0]];
-//                let all_sector_dividers = [sector_dividers[0], sector_dividers[1], sector_dividers[0]];
-//
-//                Self {
-//                    sectors: all_sectors,
-//                    sector_dividers: all_sector_dividers,
-//                    center_field
-//                }
-//            },
-//            Symmetry::ThreeAxes => {
-//                let sector = Sector::from_hash(hash_gen);
-//                let sector_divider = SectorDivider::from_hash(hash_gen);
-//
-//                let all_sectors = [sector; 6];
-//                let all_sector_dividers = [sector_divider; 3];
-//
-//                Self {
-//                    sectors: all_sectors,
-//                    sector_dividers: all_sector_dividers,
-//                    center_field
-//                }
-//            },
-//            Symmetry::Point => {
-//                let sectors: [_; 3] = array::from_fn(|_| Sector::from_hash(hash_gen));
-//                let sector_divider = SectorDivider::from_hash(hash_gen);
-//
-//                let all_sectors = [sectors[0], sectors[1], sectors[0], sectors[1], sectors[0], sectors[1]];
-//                let all_sector_dividers = [sector_divider; 3];
-//
-//                Self {
-//                    sectors: all_sectors,
-//                    sector_dividers: all_sector_dividers,
-//                    center_field
-//                }
-//            }
-//        }
-//    }
-//}
+impl FromHash for Fields {
+    fn from_hash(hash_gen: &mut HashGen) -> Self {
+        let symmetry = Symmetry::from_hash(hash_gen);
+        let center_field: Field = Field::from_hash(hash_gen);
 
-//impl FromHash for Symmetry {
-//    fn from_hash(hash_gen: &mut HashGen) -> Self {
-//        match hash_gen.gen_range_cycle(0..3) {
-//            0 => Symmetry::OneAxis,
-//            1 => Symmetry::ThreeAxes,
-//            2 => Symmetry::Point,
-//            _ => panic!("generated unfitting value")
-//        }
-//    }
-//}
+        match symmetry {
+            Symmetry::OneAxis => {
+                let sectors: [_; 3] = array::from_fn(|_| Sector::from_hash(hash_gen));
+                let sector_dividers: [_; 3] = array::from_fn(|_| SectorDivider::from_hash(hash_gen));
 
-//impl FromHash for Sector {
-//    fn from_hash(hash_gen: &mut HashGen) -> Self {
-//        let mut sector = Self::default();
-//
-//        for field in sector.0.iter_mut() {
-//            *field = Field::from_hash(hash_gen);
-//        }
-//
-//        sector
-//    }
-//}
+                let all_sectors = [sectors[0], sectors[1], sectors[2], sectors[2], sectors[1], sectors[0]];
+                let all_sector_dividers = [sector_dividers[0], sector_dividers[1], sector_dividers[0]];
 
-//impl FromHash for SectorDivider {
-//    fn from_hash(hash_gen: &mut HashGen) -> Self {
-//        let mut sector_divider = Self::default();
-//
-//        for mut _field in &mut sector_divider.0 {
-//            _field = &mut Field::from_hash(hash_gen);
-//        }
-//
-//        sector_divider
-//    }
-//}
+                Self {
+                    sectors: all_sectors,
+                    sector_dividers: all_sector_dividers,
+                    center_field
+                }
+            },
+            Symmetry::ThreeAxes => {
+                let sector = Sector::from_hash(hash_gen);
+                let sector_divider = SectorDivider::from_hash(hash_gen);
 
-//impl FromHash for Field {
-//    fn from_hash(hash_gen: &mut HashGen) -> Self {
-//        if hash_gen.gen_bool_cycle() {
-//            Self::Filled
-//        } else {
-//            Self::Empty
-//        }
-//    }   
-//}
+                let all_sectors = [sector; 6];
+                let all_sector_dividers = [sector_divider; 3];
+
+                Self {
+                    sectors: all_sectors,
+                    sector_dividers: all_sector_dividers,
+                    center_field
+                }
+            },
+            Symmetry::Point => {
+                let sectors: [_; 3] = array::from_fn(|_| Sector::from_hash(hash_gen));
+                let sector_divider = SectorDivider::from_hash(hash_gen);
+
+                let all_sectors = [sectors[0], sectors[1], sectors[0], sectors[1], sectors[0], sectors[1]];
+                let all_sector_dividers = [sector_divider; 3];
+
+                Self {
+                    sectors: all_sectors,
+                    sector_dividers: all_sector_dividers,
+                    center_field
+                }
+            }
+        }
+    }
+}
+
+impl FromHash for Symmetry {
+    fn from_hash(hash_gen: &mut HashGen) -> Self {
+        match hash_gen.gen_range_cycle(0..3) {
+            0 => Symmetry::OneAxis,
+            1 => Symmetry::ThreeAxes,
+            2 => Symmetry::Point,
+            _ => panic!("generated unfitting value")
+        }
+    }
+}
+
+impl FromHash for Sector {
+    fn from_hash(hash_gen: &mut HashGen) -> Self {
+        let mut sector = Self::default();
+
+        for field in sector.0.iter_mut() {
+            *field = Field::from_hash(hash_gen);
+        }
+
+        sector
+    }
+}
+
+impl FromHash for SectorDivider {
+    fn from_hash(hash_gen: &mut HashGen) -> Self {
+        let mut sector_divider = Self::default();
+
+        for mut _field in &mut sector_divider.0 {
+            _field = &mut Field::from_hash(hash_gen);
+        }
+
+        sector_divider
+    }
+}
+
+impl FromHash for Field {
+    fn from_hash(hash_gen: &mut HashGen) -> Self {
+        if hash_gen.gen_bool_cycle() {
+            Self::Filled
+        } else {
+            Self::Empty
+        }
+    }   
+}
 
 impl Default for Sector {
     fn default() -> Self {
--- lib/src/generate/mod.rs
+++ lib/src/generate/mod.rs
@@ -1,17 +1,3 @@
-use crate::fields::{FieldGroup, Fields, Symmetry};
-
 pub mod from_hash;
 #[cfg(feature = "rand")]
 pub mod random;
-
-pub trait FieldsGen<T: FieldGroup> {
-    fn symmetry(&mut self) -> Symmetry;
-
-    fn field_group(&mut self) -> T;
-}
-
-impl<G: FieldsGen<Fields>> Fields {
-    pub fn new(&mut gen: G) -> Self {
-
-    }
-}
