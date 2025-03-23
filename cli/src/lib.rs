eb8111866b8f3dd35bb3745b0c7aa3b3ac70203c
beklopft <ghub@giggo.de>
2025-03-23 14:42:05 +0100

    Remove filename replacement of whitespace

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -3,17 +3,11 @@ use std::path::PathBuf;
 pub fn to_file_name(string: String) -> PathBuf {
     const FILE_NAME_LENGTH: usize = 15;
 
-    let mut file_name = String::new();
-    for word in string.split_whitespace() {
-        if file_name.len() < FILE_NAME_LENGTH {
-            file_name.push('_');
-        }
-        for char in word.chars() {
-            if file_name.len() < FILE_NAME_LENGTH {
-                file_name.push(char);
-            }
-        }
-    }
+    let file_name: String = string
+        .chars()
+        .take(FILE_NAME_LENGTH)
+        .collect()
+    ;
 
     file_name.into()
 }
