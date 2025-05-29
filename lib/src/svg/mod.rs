a275db810a4970508f1fac98e75494b75e5efd9b
fantacell <ghub@giggo.de>
2025-05-29 12:00:46 +0200

    Change hard coded svg height

--- lib/src/svg/mod.rs
+++ lib/src/svg/mod.rs
@@ -9,12 +9,8 @@ use crate::fields::Fields;
 
 impl Fields {
     pub fn write_as_svg<T: Write>(self, target: T) -> Result<(), io::Error> {
-        let mut document = svg::Document::new()
-            .set("viewBox", (-50, -50, 100, 100))
-            .set("width", "100")
-            .set("height", "100")
-        ;
-        
+        let mut document = svg::Document::new().set("viewBox", (-50, -50, 100, 100));
+
         for (field_shape, degrees, orientation) in self.active_field_shapes_with_rotation() {
             let mut transform_string = format!("rotate({}, 0, 0)", degrees);
             if orientation == Orientation::Mirrored {
@@ -23,8 +19,7 @@ impl Fields {
             let path_element = svg::node::element::Path::new()
                 .set("fill", "black")
                 .set("d", field_shape.field_border_path_data().path_data)
-                .set("transform", transform_string)
-            ;
+                .set("transform", transform_string);
 
             document = document.add(path_element)
         }
@@ -40,15 +35,15 @@ pub enum FieldShape {
     SectorDividerInner,
     SectorDividerMid,
     SectorDividerOuter,
-    CenterField
+    CenterField,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Orientation {
     Original,
-    Mirrored
+    Mirrored,
 }
 
 pub struct FieldBorder {
-    path_data: svg::node::element::path::Data
+    path_data: svg::node::element::path::Data,
 }
